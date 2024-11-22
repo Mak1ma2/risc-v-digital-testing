39f09635c00ae7d3b6266559e2158dfe842429fe
Joon <jspark6@dons.usfca.edu>
2024-11-22 02:51:38 -0800

    oopsies

--- test.s
+++ test.s
@@ -1,11 +1,11 @@
-main:
+ main:
 	li a0, 2 
 	li a1, 1 
 	bge a0, a1, equal 
-	li a0, 0 
+	li a0, 3
 	ble a1, a0, finish 
-	li a0, 1  
+	li a0, 0  
 equal: 
 	ret
 finish: 
-	unimp # a0 should be 0 
+	unimp # a0 should be 3
